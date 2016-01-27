eedf195686c93b040f2dd31c580f9b9403fcdd66
rastating <rob@rastating.com>
2016-01-27 20:34:25 +0000

    Add js_post script

---
+++ data/js/post.js
@@ -0,0 +1,20 @@
+function post(path, params, method) {
+  method = method || "post";
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
+
+  for(var key in params) {
+    if(params.hasOwnProperty(key)) {
+      var hiddenField = document.createElement("input");
+      hiddenField.setAttribute("type", "hidden");
+      hiddenField.setAttribute("name", key);
+      hiddenField.setAttribute("value", params[key]);
+
+      form.appendChild(hiddenField);
+     }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
